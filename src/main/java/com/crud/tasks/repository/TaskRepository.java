41328b4a00d016177867fbb25fb893ed6f53407f
Bauaser <michal.bala.1989@gmail.com>
2021-01-13 22:41:59 +0100

    Problem 19.2 find task by id crud repository Db

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -4,6 +4,10 @@ import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
+List<Task> findAll();
+Task findById(long id);
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,13 +1,23 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-public class DbService {
-private final TaskRepository repository;
+import java.util.List;
 
-    public DbService(TaskRepository repository) {
-        this.repository = repository;
+@Service
+@RequiredArgsConstructor
+public class DbService {
+    private final TaskRepository repository;
+
+    public List<Task> getAllTasks(){
+        return repository.findAll();
     }
+
+    public Task getTasksById(final long id){
+        return repository.findById(id);
+    }
+
 }
