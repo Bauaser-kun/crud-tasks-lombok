dcdc260ccff46f21834f4c30998632624124a209
Bauaser <michal.bala.1989@gmail.com>
2021-02-04 14:36:57 +0100

    Problem 22.2 Trello app refactoring

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -18,20 +18,29 @@ public class TrelloClient {
 
     @Value("${trello.api.endpoint.prod}")
     private String trelloApiEndpoint;
+
     @Value("${trello.app.key}")
     private String trelloAppKey;
+
     @Value("${trello.app.token}")
     private String trelloToken;
 
-    public List<TrelloBoardDto> getTrelloBoards() {
-        URI url = UriComponentsBuilder.fromHttpUrl(
-                trelloApiEndpoint +trelloApiEndpoint + "/members/michabaa/boards")
-                .queryParam("key", trelloAppKey)
-                .queryParam("token", trelloToken)
+    @Value("${trello.app.username}")
+    private String trelloUsername;
+
+    private URI buildURL (String endpoint, String key, String token, String username){
+        return UriComponentsBuilder.fromHttpUrl(
+                endpoint + "/members/" + username + "/boards")
+                .queryParam("key", key)
+                .queryParam("token", token)
                 .queryParam("fields", "name,id")
                 .build()
                 .encode()
                 .toUri();
+    }
+
+    public List<TrelloBoardDto> getTrelloBoards() {
+        URI url = buildURL(trelloApiEndpoint, trelloAppKey, trelloToken, trelloUsername);
 
         TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
 
