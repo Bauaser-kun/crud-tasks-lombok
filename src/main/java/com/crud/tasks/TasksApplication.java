0141ee8d67b28ff8fdb9c23943850977fb139725
Bauaser <michal.bala.1989@gmail.com>
2021-02-11 14:14:11 +0100

    problem 23.1

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -15,8 +15,8 @@ public class TasksApplication /*extends SpringBootServletInitializer*/ {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
-	/*@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TasksApplication.class);
-	}*/
+
+	//protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	//	return application.sources(TasksApplication.class);
+	//}
 }
